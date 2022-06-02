c79e37435dd0075ccb80f94553ed4cc53c18774c
swsong <ssw890720@gmail.com>
2022-06-03 00:33:03 +0900

    로그추적기 v1 테스트코드 추가

--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
@@ -9,8 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloTraceV1 {
 
-    private static final String START_PREFIX = "-->";
-    private static final String COMPLETE_PREFIX = "<--";
+    private static final String START_PREFIX = "-->";private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
 
     public TraceStatus begin(String message) {
