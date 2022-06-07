f52451f58ba421d1faa41fb240e2f659ca649164
swsong <ssw890720@gmail.com>
2022-06-07 23:37:28 +0900

    로그추적기 v2 적용

---
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderServiceve;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/req")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("orderController.request()");
+            orderServiceve.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
+        return "ok";
+    }
+
+}
