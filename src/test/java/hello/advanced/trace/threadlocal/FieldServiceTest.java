d317c1ba1acc5bf8172901e0d98aad6f8c04dcae
swsong <ssw890720@gmail.com>
2022-06-11 23:56:21 +0900

    동시성 문제 예제 코드추가

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+    
+    @Test
+    public void field() throws Exception {
+        log.info("실행");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        //각각의 로직을 실행
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        //동시성 문제가 발생하지 않는다.
+        threadA.start(); // A로직 실행
+        sleep(2000); // 2초를쉰다
+        threadB.start(); // B 시작
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
