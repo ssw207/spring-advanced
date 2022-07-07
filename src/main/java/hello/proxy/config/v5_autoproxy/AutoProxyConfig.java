e07c2d9fd76c3bbda4aeadd4e5218a14ed9d6d9f
swsong <ssw890720@gmail.com>
2022-07-07 23:49:11 +0900

    오토 프록시 적용

---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,27 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTracedAdvice;
+import hello.trace.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+    // 스프링의 자동 프록시생성기가 있으므로 advisor만 등록하면 자동으로 프록시를 생성한다.
+    @Bean
+    public Advisor getAdvisor1(LogTrace logTrace) {
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*","order*", "save*");
+
+        LogTracedAdvice advice = new LogTracedAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
