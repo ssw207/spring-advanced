b80ae508b7e5da30df8446189e2b40545e77ef9e
swsong <ssw890720@gmail.com>
2022-07-24 23:29:53 +0900

    포인트컷 표현식 예제

--- src/main/java/hello/AdvancedApplication.java
+++ src/main/java/hello/AdvancedApplication.java
@@ -1,12 +1,10 @@
 package hello;
 
-import hello.aop.order.aop.AspectV6Advice;
 import hello.trace.trace.logtrace.LogTrace;
 import hello.trace.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class) // @Configuration 명시적으로 설정파일을 등록할때 사용
 //@Import({AppV1Config.class, AppV2Config.class})
@@ -26,7 +24,7 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
 //@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
-@Import(AspectV6Advice.class)
+//@Import(AspectV6Advice.class)
 @SpringBootApplication(scanBasePackages = {"hello.aop.order", "hello.aop.member"}) // SpringBootApplication 컴포넌트 스캔의 기본위치는 현재 클래스 기준으로 스캔한다.
 public class AdvancedApplication {
 
